722d1ef96dc9485471ee24c421986a5b60d295cf
benjaminmiharris <benjmiharris@gmail.com>
2023-05-02 17:21:35 +0300

    base url added

--- next.config.js
+++ next.config.js
@@ -7,5 +7,6 @@ module.exports = {
   ...nextConfig, // Merge the two objects
   env: {
     VIMEO_TOKEN: "e0422dee75d6639713b5eea531b8a41a",
+    NEXT_PUBLIC_BASE_URL: "https://main.d2z866ovmu3pci.amplifyapp.com",
   },
 };
