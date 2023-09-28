42bc94cd2a97eabb30ba176831ee58300be7f025
benjaminmiharris <benjmiharris@gmail.com>
2023-09-28 13:20:38 +0300

    tester push

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
-export const LOCAL_DEV = "http://localhost:3000";
+// export const LOCAL_DEV = "http://localhost:3000";
 
-// export const LOCAL_DEV = "https://main.d2z866ovmu3pci.amplifyapp.com";
+export const LOCAL_DEV = "https://main.d2z866ovmu3pci.amplifyapp.com";
 
 export const PASSWORD = "Aish613!";
