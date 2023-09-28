8352d195ed486f2783ddaff408e9edb830f8b20a
benjaminmiharris <benjmiharris@gmail.com>
2023-09-28 13:26:00 +0300

    env router issues

--- next.config.js
+++ next.config.js
@@ -7,9 +7,8 @@ module.exports = {
   ...nextConfig, // Merge the two objects
   env: {
     NEXT_PUBLIC_VIMEO_TOKEN: "e0422dee75d6639713b5eea531b8a41a",
-    NEXT_PUBLIC_TESTER: "HeyThere",
     NEXT_PUBLIC_SITE_PASSWORD: "Aish613!",
-    // NEXT_PUBLIC_DOMAIN: "https://main.d2z866ovmu3pci.amplifyapp.com",
-    NEXT_PUBLIC_DOMAIN: "http://localhost:3000",
+    NEXT_PUBLIC_DOMAIN: "https://main.d2z866ovmu3pci.amplifyapp.com",
+    // NEXT_PUBLIC_DOMAIN: "http://localhost:3000",
   },
 };
