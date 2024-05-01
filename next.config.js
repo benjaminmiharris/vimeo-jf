9359224d1bb46e9c66839b0aa24b5257693fef54
benjaminmiharris <benjmiharris@gmail.com>
2024-05-01 12:09:03 +0300

    production

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   env: {
     NEXT_PUBLIC_VIMEO_TOKEN: "870bd1b9ab9d7eb6cd066b9864ea37ae",
     NEXT_PUBLIC_SITE_PASSWORD: "Aish613!",
-    // NEXT_PUBLIC_DOMAIN: "https://main.d2z866ovmu3pci.amplifyapp.com",
-    NEXT_PUBLIC_DOMAIN: "http://localhost:3000",
+    NEXT_PUBLIC_DOMAIN: "https://main.d2z866ovmu3pci.amplifyapp.com",
+    // NEXT_PUBLIC_DOMAIN: "http://localhost:3000",
   },
 };
